e9850452492839354a8e88a940cb22eacc66aa28
KelmerWilder <60844084@continental.edu.pe>
2026-06-12 12:56:43 -0500

    actu codigo, abajo todo

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-print("Tercer intento, este archivo está conectado con Git y GitHub. Ya no subas JP")
+print("ya muchos intentos, este archivo está conectado con Git y GitHub. Ya no subas JP")
 print("fujimori nunca más")
+print("No deseo JP Y FP")
